2ec21b1622f60ec7fcb942dc726bed2f24cfb34d
GianlucaA18 <imanol.milla@gmail.com>
2024-10-07 10:59:30 -0500

    Agregar archivos de proyecto.

---
+++ ABV VIAJES/Views/Master.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ABV_VIAJES.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ABV_VIAJES
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnViajes_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Viajes());
+
+        }
+
+        private async void btnRestaurantes_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Restaurantes());
+        }
+
+        private async void btnBares_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Bares());
+        }
+
+        private async void btnHoteles_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Hoteles());
+        }
+
+        private async void btnCompras_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Compras());
+        }
+    }
+}
